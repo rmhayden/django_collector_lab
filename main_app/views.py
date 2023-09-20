7ed3fb9446af32c77b23f7f96f037c76dc010ac0
rmhayden <robertmorsehayden@gmail.com>
2023-09-20 11:49:15 -0400

    added view all finches page

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+
+# data usually comes from a model; temporary data below for now
+
+finches = [
+    {'name': 'darwin'},
+    {'name': 'big bird'}
+]
+
 # Create your views here.
 # (views in Django are like controllers in express)
 
@@ -9,3 +17,10 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
+# the {} argument is how python data is passed into this function
