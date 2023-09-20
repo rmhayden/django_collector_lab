ee2b181cab861147e8255884a50b35c18d584686
rmhayden <robertmorsehayden@gmail.com>
2023-09-20 11:34:53 -0400

    home page and about page created

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+# (views in Django are like controllers in express)
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
