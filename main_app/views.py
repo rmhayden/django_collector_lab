671cbf666c57325418b775577c25b72ed6e4c9d3
rmhayden <robertmorsehayden@gmail.com>
2023-09-21 14:44:17 -0400

    part 2 functionality all added

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
+from .models import Finch
 
 # data usually comes from a model; temporary data below for now
 
-finches = [
-    {'name': 'darwin'},
-    {'name': 'big bird'}
-]
+# finches = [
+#     {'name': 'darwin'},
+#     {'name': 'big bird'}
+# ]
 
 # Create your views here.
 # (views in Django are like controllers in express)
@@ -20,7 +21,16 @@ def about(request):
 
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch': finch
+    })
+
 # the {} argument is how python data is passed into this function
+
